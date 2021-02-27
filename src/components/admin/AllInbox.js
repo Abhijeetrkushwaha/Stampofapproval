03e7fe1d4b29dd7dc8ba26a8bf46a3a13233929c
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2021-02-27 10:53:23 +0530

    added data

--- src/components/admin/AllInbox.js
+++ src/components/admin/AllInbox.js
@@ -9,11 +9,11 @@ function AllInbox({ documents }) {
         <Card key={data.id}>
           <Card.Header className="d-flex justify-content-between align-items-center">
             <span className="font-weight-bold">{`Message from ${data.document.userInfo.userName} ${data.document.userInfo.lastName}`}</span>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            <Accordion.Toggle as={Button} variant="link" eventKey={data.id}>
               View Message
             </Accordion.Toggle>
           </Card.Header>
-          <Accordion.Collapse eventKey="0">
+          <Accordion.Collapse eventKey={data.id}>
             <Card.Body>Hello! I'm the body</Card.Body>
           </Accordion.Collapse>
         </Card>
