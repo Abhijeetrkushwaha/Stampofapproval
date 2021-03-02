f070664a0a781b2cd031a8d6e942756176560841
Abhijeetrkushwaha <abhijeetrkushwaha28@gmail.com>
2021-03-02 19:50:01 +0530

    added data

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -12,7 +12,7 @@ function Admin({user}) {
   const handleSubmit = e => {
     e.preventDefault()
     if(email.trim().length > 0 && password.trim().length > 0){
-      if(email === '12345678' && password === '12345678') {
+      if(email === process.env.REACT_APP_ADMIN_PASSWORD && password === process.env.REACT_APP_ADMIN_PASSWORD) {
         setValid(true)
       }
       setErrorMessage('Invalid email or password')
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBLULt1U448mgK7NlYUiG3o8SrtT2_AvMI",
-  authDomain: "stamp-of-approval-43eda.firebaseapp.com",
-  projectId: "stamp-of-approval-43eda",
-  storageBucket: "stamp-of-approval-43eda.appspot.com",
-  messagingSenderId: "765965211731",
-  appId: "1:765965211731:web:6459e5b34ee681ad1c8ea6",
-  measurementId: "G-15YSDHSTLG"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDING_ID,
+  appId: process.env.REACT_APP_API_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 })
 
 const db = firebaseApp.firestore();
